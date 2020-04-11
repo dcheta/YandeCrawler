e1b6069057dcb26bc34dae067f071c81079d8dc1
ctzn <37342598+dcheta@users.noreply.github.com>
2020-04-11 15:44:10 +0800

    Update YandeCrawler.py

--- YandeCrawler.py
+++ YandeCrawler.py
@@ -12,22 +12,22 @@ def get_url(main_url):
         url_ls.append('https://yande.re/post/show/' + id)
     return(url_ls)
 
-def download(url,file_name,i):
+def download(url,image_name,i):
     print('第%d张图片正在下载' %(i))
     response = requests.get(url)
     result = response.text
     start=result.find('https://files.yande.re/image')
     end=result.find('\"',start)
-    large_url=result[start:end]
+    large_url=result[start:end]#获取高清图片的url
     response = requests.get(large_url)
     img = response.content
-    with open(file_name+'.'+large_url.split('.')[-1], 'wb') as f:
+    with open(image_name+'.'+large_url.split('.')[-1], 'wb') as f:#split分割获取图片格式后缀
         f.write(img)
     print('第%d张图片已完成下载' %(i))
 
 if __name__ == '__main__':
     start_page=1
-    end_page=1
+    end_page=10
     tags='sakimichan'
     num_thread=10
     url_ls=[]
@@ -36,11 +36,12 @@ if __name__ == '__main__':
         url_ls.extend(get_url(main_url))
         print('第%d页的图片链接已提取' %(i+start_page))
     print('一共%d张图片' %(len(url_ls)))
-    if not os.path.exists(tags):
-        os.mkdir(tags)
+    file_name=tags+str(start_page)+'-'+str(end_page)#文件夹名字，默认与py文件在同一文件夹
+    if not os.path.exists(file_name):
+        os.mkdir(file_name)
     p = futures.ThreadPoolExecutor(max_workers=num_thread)
     for url in url_ls:
-        file_name=tags+'/'+str(url_ls.index(url)+1)
-        future=p.submit(download, url, file_name, url_ls.index(url)+1)
+        image_name=file_name+'/'+str(url_ls.index(url)+1)
+        future=p.submit(download, url, image_name, url_ls.index(url)+1)
     p.shutdown()
     print("下载完成")
